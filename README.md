4b748b3f9d34278fdcd8c08aada8a0ec9e2e954f
Stephen Fox <stephen.j.fox.jr@gmail.com>
2024-08-17 15:03:27 -0400

    general: Unlocked screen logic.
    This commit also adds support for stopping previously-started
    child processes when a new event occurs.

--- README.md
+++ README.md
@@ -2,17 +2,26 @@
 
 waked executes programs when macOS resumes from sleep. By default,
 it executes all programs found in directory-path. If directory-path
-is not specified, then '/usr/local/waked' is used.
+is not specified, then `/usr/local/etc/waked` is used.
+
+Executables containing '-on-unlock' in their name will only be executed
+once the screen is unlocked.
+
+waked will continuously re-execute a program if it exits with a non-zero
+exit status.
 
 ## Example
 
 ```console
+$ # Using the default directory:
+$ waked
+$ # Alternatively, specify a custom directory:
 $ waked ~/.waked/
 ```
 
 ## Installation
 
-1. `xcode-select --install`
+1. `xcode-select --install` (I know, unfortunate)
 2. `go install gitlab.com/stephen-fox/waked@latest`
 3. `sudo cp ~/go/bin/waked /usr/local/bin/`
 
@@ -24,3 +33,29 @@ $ waked ~/.waked/
 ## Stop daemon
 
 `launchctl unload -w ~/Library/LaunchAgents/com.gitlab.stephen-fox.waked.plist`
+
+## Custom screen unlock check logic
+
+If you would like to implement your own screen unlock checking logic in
+a shell script, you can use this shell function:
+
+```sh
+# screen_is_locked is based on work by Joel Bruner:
+# https://stackoverflow.com/a/66723000
+#
+# For an Objective-C implementation, see:
+# https://stackoverflow.com/a/76241560
+#
+# This version avoids creating temporary files (via '<<<') by using plutil.
+#
+# Returns 0 if screen is locked.
+screen_is_locked() {
+  [ "$(/usr/sbin/ioreg -n Root -d1 -a \
+    | /usr/bin/plutil \
+      -extract 'IOConsoleUsers.0.CGSSessionScreenIsLocked' \
+      raw -)" \
+    == 'true' ]
+
+  return $?
+}
+```
--- main.go
+++ main.go
@@ -2,15 +2,21 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"context"
 	"errors"
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
 	"os/signal"
 	"path/filepath"
 	"runtime"
+	"strings"
+	"sync"
 	"syscall"
 	"time"
 
@@ -28,16 +34,23 @@ SYNOPSIS
   ` + appName + ` [options] [directory-path]
 
 DESCRIPTION
-  waked executes programs when macOS resumes from sleep. By default,
+  ` + appName + ` executes programs when macOS resumes from sleep. By default,
   it executes all programs found in directory-path. If directory-path
   is not specified, then '` + defaultExesDirPath + `' is used.
+
+  Executables containing '` + needsUnlockStr + `' in their name will only be executed
+  once the screen is unlocked.
+
+  ` + appName + ` will continuously re-execute a program if it exits with a non-zero
+  exit status.
 
 OPTIONS
 `
 
 	helpArg = "h"
 
-	defaultExesDirPath = "/usr/local/" + appName
+	defaultExesDirPath = "/usr/local/etc/" + appName
+	needsUnlockStr     = "-on-unlock"
 )
 
 func main() {
@@ -115,10 +128,11 @@ func mainWtihError() error {
 	return nil
 }
 
-// TODO: Cancel existing execs if called again.
 type execCtl struct {
-	ctx     context.Context
-	exesDir string
+	ctx            context.Context
+	exesDir        string
+	mu             sync.Mutex
+	stopChildrenFn func()
 }
 
 func (o *execCtl) validate() error {
@@ -136,6 +150,9 @@ func (o *execCtl) validate() error {
 }
 
 func (o *execCtl) onEvent(foundation.Notification) {
+	o.mu.Lock()
+	defer o.mu.Unlock()
+
 	infos, err := os.ReadDir(o.exesDir)
 	if err != nil {
 		log.Printf("failed to read executables directory %q - %s",
@@ -144,6 +161,13 @@ func (o *execCtl) onEvent(foundation.Notification) {
 		return
 	}
 
+	if o.stopChildrenFn != nil {
+		o.stopChildrenFn()
+	}
+
+	ctx, cancelFn := context.WithCancel(o.ctx)
+	o.stopChildrenFn = cancelFn
+
 	for _, info := range infos {
 		if info.IsDir() {
 			continue
@@ -151,38 +175,167 @@ func (o *execCtl) onEvent(foundation.Notification) {
 
 		exePath := filepath.Join(o.exesDir, info.Name())
 
-		err := execRetry(o.ctx, exePath)
-		if err != nil {
-			log.Printf("[%s] failed to execute - %s", exePath, err)
-		}
+		go execRetry(ctx, exePath)
 	}
 }
 
 func execRetry(ctx context.Context, exePath string) error {
+	for {
+		_, err := os.Stat(exePath)
+		if err != nil {
+			log.Printf("[%s] no longer stat'able - %s", exePath, err)
+
+			return err
+		}
+
+		err = execOnce(ctx, exePath)
+		switch err {
+		case nil:
+			return nil
+		default:
+			log.Printf("[%s] %s", exePath, err)
+		}
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			continue
+		}
+	}
+}
+
+func execOnce(ctx context.Context, exePath string) error {
+	if strings.Contains(filepath.Base(exePath), needsUnlockStr) {
+		log.Println("TODO - needs unlock")
+
+		isLocked, err := checkIfLocked(ctx)
+		switch {
+		case isLocked:
+			select {
+			case <-ctx.Done():
+				return fmt.Errorf("context canceled while checking if screen locked - %w",
+					ctx.Err())
+			case <-time.After(5 * time.Second):
+				return fmt.Errorf("screen was locked, will retrying...")
+			}
+		case err != nil:
+			log.Printf("failed to determine if screen is locked - %s", err)
+		}
+	}
+
 	ctx, cancelFn := context.WithTimeoutCause(
 		ctx,
 		10*time.Minute,
-		errors.New("timed out waiting for child process to finish"))
+		errors.New("timed-out waiting for child process to exit"))
 	defer cancelFn()
 
-	for {
-		exe := exec.CommandContext(ctx, exePath)
+	exe := exec.CommandContext(ctx, exePath)
 
-		exe.Stderr = os.Stderr
-		exe.Stdout = os.Stdout
+	stderr := newExeLogger(exePath)
+	defer stderr.Close()
 
-		err := exe.Run()
-		if err != nil {
-			log.Printf("[%s] retrying... (reason: %s)", exePath, err)
+	stdout := newExeLogger(exePath)
+	defer stdout.Close()
 
-			select {
-			case <-ctx.Done():
-				return ctx.Err()
-			case <-time.After(time.Minute):
-				continue
-			}
+	exe.Stderr = stderr
+	exe.Stdout = stdout
+
+	err := exe.Run()
+	if err != nil {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-time.After(10 * time.Second):
+			return fmt.Errorf("process failed (reason: %s), retrying...", err)
 		}
-
-		return nil
 	}
+
+	return nil
+}
+
+func newExeLogger(exePath string) *exeLogger {
+	r, w := io.Pipe()
+
+	l := &exeLogger{
+		exePath: exePath,
+		r:       r,
+		w:       w,
+	}
+
+	go l.loop()
+
+	return l
+}
+
+type exeLogger struct {
+	exePath string
+	r       io.ReadCloser
+	w       io.WriteCloser
+}
+
+func (o *exeLogger) Write(b []byte) (int, error) {
+	return o.w.Write(b)
+}
+
+func (o *exeLogger) Close() error {
+	o.r.Close()
+	o.w.Close()
+
+	return nil
+}
+
+func (o *exeLogger) loop() {
+	scanner := bufio.NewScanner(o.r)
+
+	for scanner.Scan() {
+		log.Printf("[%s] %s", o.exePath, scanner.Text())
+	}
+}
+
+// Based on work by Joel Bruner:
+// https://stackoverflow.com/a/66723000
+//
+// We could use Go's XML parser here, but I do not feel
+// like dealing with Apple's plist format.
+func checkIfLocked(ctx context.Context) (bool, error) {
+	// /usr/sbin/ioreg -n Root -d1 -a
+	ioreg := exec.CommandContext(
+		ctx,
+		"/usr/sbin/ioreg",
+		"-n",
+		"Root",
+		"-d1",
+		"-a")
+
+	ioregOutput, err := ioreg.CombinedOutput()
+	if err != nil {
+		return false, fmt.Errorf("ioreg failed (%v) - %w - output: %q",
+			ioreg.Args, err, ioregOutput)
+	}
+
+	const coreGraphicsParam = "CGSSessionScreenIsLocked"
+
+	if !bytes.Contains(ioregOutput, []byte(coreGraphicsParam)) {
+		return false, nil
+	}
+
+	// /usr/bin/plutil -extract 'IOConsoleUsers.0.CGSSessionScreenIsLocked' raw -
+	plutil := exec.CommandContext(
+		ctx,
+		"/usr/bin/plutil",
+		"-extract",
+		"IOConsoleUsers.0."+coreGraphicsParam,
+		"raw",
+		"-")
+
+	plutil.Stdin = bytes.NewReader(ioregOutput)
+
+	plutilOutput, err := plutil.CombinedOutput()
+	if err != nil {
+		return false, fmt.Errorf("plutil (%v) failed - %w - output: %q",
+			plutil.Args, err, plutilOutput)
+	}
+
+	return bytes.Equal([]byte("true"), plutilOutput), nil
 }
